c000af63a70c964b67607f9a60137d6ac2cb766b
Alex Lux <alexlux@live.com>
2023-02-17 16:48:01 -0800

    setting up the project

---
+++ React/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Container, Grid } from "@material-ui/core";
+import React from "react";
+
+function App() {
+  return (
+    <Container>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          style={{ backgroundColor: "blue" }}
+        >
+          abc
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "red" }}>
+          xyz
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;
